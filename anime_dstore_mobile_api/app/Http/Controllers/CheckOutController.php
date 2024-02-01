a2ed92dc1edff6af2837414e0769b3ad739f9bc1
visal1234 <bzhou@paragoniu.edu.kh>
2024-02-02 03:07:21 +0700

    update checkout

--- anime_dstore_mobile_api/app/Http/Controllers/CheckOutController.php
+++ anime_dstore_mobile_api/app/Http/Controllers/CheckOutController.php
@@ -2,31 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\History;
+use App\Models\CartItem;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
     public function checkout(Request $request)
     {
-
-        try{
+        try {
+            // Validate the request data
+            // $request->validate([
+            //     'user_id' => 'required|exists:users,id',
+            //     'cart' => 'required|array',
+            //     'cart.*.item_id' => 'required|exists:items,id',
+            //     'cart.*.quantity' => 'required|integer|min:1',
+            // ]);
             $user_id = $request->input('user_id');
-            $cart = $request->input('cart'); 
+            $cart = $request->input('cart');
+            foreach ($cart as $item) {
+                dd($item);
+                History::create([
+                    'user_id' => $user_id,
+                    'item_id' => $item['item_id'],
+                    'quantity' => $item['quantity'],
+                ]);
+            }
+            
+
             $response = [
                 'user_id' => $user_id,
                 'cart' => $cart,
                 'message' => 'Checkout successful.',
             ];
-            return response()->json($response, 200);
-    } 
-    catch (\Exception $e) {
-        $response = [
-            'success' => false,
-            'error' => $e->getMessage(),
-            'message' => 'Search failed.'
-        ];
 
-        return response()->json($response, 400); 
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            $response = [
+                'success' => false,
+                'error' => $e->getMessage(),
+                'message' => 'Checkout failed.',
+            ];
+
+            return response()->json($response, 400);
+        }
     }
-}
 }
